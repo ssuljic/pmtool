619e1ddf844ca48ccddc493dca71dc21a404a6a4
sadzid <sadzid.etf@gmail.com>
2014-12-20 17:42:07 +0100

    Added edit and create for projects and activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -27,8 +27,30 @@ class ActivitiesController < BaseController
 		@activity = Activity.find(params[:id])
 	end
 
+	def new
+		@activity = Activity.new
+	end
+
+	def create
+		@activity = Activity.new(activity_params)
+		@activity.project = Project.find(params[:project_id])
+		@activity.save!
+
+		redirect_to project_activities_path(params[:project_id])
+	end
+
+	def update
+		Activity.find(params[:id]).update(activity_params)
+		redirect_to project_activities_path(params[:project_id])
+	end
+
 	def destroy
 		Activity.destroy(params[:id])
 		redirect_to project_activities_path(Project.find(params[:project_id]))
+	end
+
+	private
+	def activity_params
+		params.require(:activity).permit(:name, :description, :duration, :finished)
 	end
 end
