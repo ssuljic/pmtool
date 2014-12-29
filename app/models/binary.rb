e844ecd519a3b7a0085ee058f3beff24fb247a8d
sadzid <sadzid.etf@gmail.com>
2014-12-29 22:46:19 +0100

    binread

--- app/models/binary.rb
+++ app/models/binary.rb
@@ -2,11 +2,11 @@ class Binary < ActiveRecord::Base
   has_many :uploads
   
   def file_data=(input_data)
-    self.data = input_data.read
-    puts input_data.read
-    puts input_data.tempfile.read
-    puts input_data
-    puts IO.binread(input_data.tempfile.path)
-    puts input_data.tempfile.size
+    self.data = IO.binread(input_data.tempfile.path)
+    # puts input_data.read
+    # puts input_data.tempfile.read
+    # puts input_data
+    # puts IO.binread(input_data.tempfile.path)
+    # puts input_data.tempfile.size
   end
 end
